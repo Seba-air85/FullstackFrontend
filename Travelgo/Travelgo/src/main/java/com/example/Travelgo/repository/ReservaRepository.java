00c10eb376941fbae5e84c4086ad071eb6d7b7e1
Seba-air85 <se.aird@duocuc.cl>
2025-11-28 17:17:06 -0300

    Delete Travelgo/Travelgo directory

--- Travelgo/Travelgo/src/main/java/com/example/Travelgo/repository/ReservaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Travelgo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Travelgo.model.Reserva;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {}
